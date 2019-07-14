bed5b625d380a35826a598ecd008c9577a3a81b2
A.C.M <undertakers_help@yahoo.com>
2019-07-14 16:21:48 +0200

    New Release 2.3

--- rice.lua
+++ rice.lua
@@ -1,6 +1,6 @@
 minetest.register_decoration({
 	deco_type = "simple",
-	place_on = {"default:dirt_with_rainforest_litter", "default:dirt"},
+	place_on = {"default:dirt_with_rainforest_litter", "default:dirt", "default:dirt_with_grass"},
 	spawn_by = {"default:water_source", "default:river_water_source", "default:water_flowing", "default:river_water_flowing", "default:papyrus", "default:jungletree"},
 	--biomes = {"rainforest", "rainforest_swamp", "rainforest_ocean", "grassland_ocean", "deciduous_forest_shore", "deciduous_forest_ocean", "coniferous_forest_ocean"},
 	sidelen = 32,
