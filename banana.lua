4ad88bf81f8797c5a2ff4ece0728e6e4bebfb5c5
acmgit <undertakers_help@yahoo.com>
2022-02-12 17:45:05 +0100

    Tuning again

--- banana.lua
+++ banana.lua
@@ -1,6 +1,6 @@
 minetest.register_decoration({
 	deco_type = "simple",
-	place_on = {"default:dirt_with_rainforest_litter"},
+	place_on = {"default:dirt_with_rainforest_litter", "default:dirt_with_leaves"},
 	spawn_by = {"default:jungletree"},
 	num_spawn_by = 2,
 	sidelen = 32,
